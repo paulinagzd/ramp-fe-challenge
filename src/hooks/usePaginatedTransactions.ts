2cda5bb589c1f7acdaca7dfe82f60059422d68ca
paulinagzd <paulinagdavalos@gmail.com>
2023-07-24 16:29:02 -0600

    Bug 7 - Persisting transaction approved values

--- src/hooks/usePaginatedTransactions.ts
+++ src/hooks/usePaginatedTransactions.ts
@@ -4,13 +4,17 @@ import { PaginatedTransactionsResult } from "./types"
 import { useCustomFetch } from "./useCustomFetch"
 
 export function usePaginatedTransactions(): PaginatedTransactionsResult {
-  const { fetchWithCache, loading } = useCustomFetch()
+  // Bug 7
+  // Simple fix: fetching without cache. This allows requests to be made when a filter
+  // value is changed. A nice-to-have would have been access to the API calls and manage
+  // pertaining the information when the page is refreshed.
+  const { fetchWithoutCache, loading } = useCustomFetch()
   const [paginatedTransactions, setPaginatedTransactions] = useState<PaginatedResponse<
     Transaction[]
   > | null>(null)
 
   const fetchAll = useCallback(async () => {
-    const response = await fetchWithCache<PaginatedResponse<Transaction[]>, PaginatedRequestParams>(
+    const response = await fetchWithoutCache<PaginatedResponse<Transaction[]>, PaginatedRequestParams>(
       "paginatedTransactions",
       {
         page: paginatedTransactions === null ? 0 : paginatedTransactions.nextPage,
@@ -24,7 +28,7 @@ export function usePaginatedTransactions(): PaginatedTransactionsResult {
 
       return { data: response.data, nextPage: response.nextPage }
     })
-  }, [fetchWithCache, paginatedTransactions])
+  }, [fetchWithoutCache, paginatedTransactions])
 
   const invalidateData = useCallback(() => {
     setPaginatedTransactions(null)
--- src/hooks/useTransactionsByEmployee.ts
+++ src/hooks/useTransactionsByEmployee.ts
@@ -3,13 +3,17 @@ import { RequestByEmployeeParams, Transaction } from "../utils/types"
 import { TransactionsByEmployeeResult } from "./types"
 import { useCustomFetch } from "./useCustomFetch"
 
+  // Bug 7
+  // Simple fix: fetching without cache. This allows requests to be made when a filter
+  // value is changed. A nice-to-have would have been access to the API calls and manage
+  // pertaining the information when the page is refreshed.
 export function useTransactionsByEmployee(): TransactionsByEmployeeResult {
-  const { fetchWithCache, loading } = useCustomFetch()
+  const { fetchWithoutCache, loading } = useCustomFetch()
   const [transactionsByEmployee, setTransactionsByEmployee] = useState<Transaction[] | null>(null)
 
   const fetchById = useCallback(
     async (employeeId: string) => {
-      const data = await fetchWithCache<Transaction[], RequestByEmployeeParams>(
+      const data = await fetchWithoutCache<Transaction[], RequestByEmployeeParams>(
         "transactionsByEmployee",
         {
           employeeId,
@@ -18,7 +22,7 @@ export function useTransactionsByEmployee(): TransactionsByEmployeeResult {
 
       setTransactionsByEmployee(data)
     },
-    [fetchWithCache]
+    [fetchWithoutCache]
   )
 
   const invalidateData = useCallback(() => {
